642dd766ad5af04a5b54c7dc7e0eb602d2d2accd
FIRAT <frtdkmn@gmail.com>
2023-03-26 10:23:13 +0300

    First Commit

---
+++ Lesson02/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>What is your name?</p>
+      <button>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
