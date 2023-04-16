514742c3708aa40c6d72e2bd499ead9a139444bd
FIRAT <frtdkmn@gmail.com>
2023-04-16 16:01:20 +0300

    BUG FIXED for RTK-Redux Lesson.

--- Lesson12/redux-formapp/src/app/store.js
+++ Lesson12/redux-formapp/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import usersReducer from '../features/users/usersSlice';
 
 //STORE OLUŞTURMAK İÇİN KULLANILIR.
 //ConfigureStore; size reducer konfigurasyon etmek için kullanılır.
@@ -7,5 +8,6 @@ import counterReducer from '../features/counter/counterSlice';
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    users: usersReducer,
   },
 });
--- Lesson12/redux-formapp/src/features/users/Users.js
+++ Lesson12/redux-formapp/src/features/users/Users.js
@@ -5,10 +5,11 @@ import { selectUsers, fetchUsers } from './usersSlice'
 export default function Users() {
     const users = useSelector(selectUsers)
     const dispatch = useDispatch()
+    console.log("users=>",users);
   return (
     <div>Users
         <ul>
-            {users && users.map((user,index) => (
+            {users.users && users.users.map((user,index) => (
                 <li key={index}>{index+1}.{user.name}</li>
             ))}
         </ul>
